2fea5c0d59117953819f5f172849ae10481a0ff3
ShishantG87 <shishantbohora44@gmail.com>
2025-03-03 16:08:45 -0500

    changes

--- scripts/include-components.js
+++ scripts/include-components.js
@@ -3,12 +3,7 @@ fetch('components/header.html')
     .then(response => response.text())  
     .then(data => {
        
-        document.getElementById('header').innerHTML = data;
-    })
-    .catch(error => {
-        
-        document.getElementById('header').innerHTML = "Cant load header.";
-        console.error("Error loading HTML file:", error);
+        document.getElementsByTagName('header')[0].innerHTML = data;
     });
 
 
@@ -17,10 +12,5 @@ fetch('components/footer.html')
 .then(response => response.text())  
 .then(data => {
    
-    document.getElementById('footer').innerHTML = data;
-})
-.catch(error => {
-    
-    document.getElementById('footer').innerHTML = "Cant load footer.";
-    console.error("Error loading HTML file:", error);
+    document.getElementsByTagName('footer')[0].innerHTML = data;
 });
